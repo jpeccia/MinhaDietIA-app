946e51f1b9f72a4891ceb50fa984669d2e3573f5
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-10 17:06:10 -0300

    feat: add controller react hook form and props in input component

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,11 +1,32 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput, KeyboardTypeOptions } from 'react-native'
 import React from 'react'
+import { Controller } from 'react-hook-form'
 
-export default function index() {
+interface InputProps{
+    name: string;
+    control: any;
+    placeholder?: string;
+    rules?: object,
+    error?: string;
+    keyboardType: KeyboardTypeOptions;
+}
+
+export default function index({name, control, placeholder, rules, error, keyboardType}: InputProps) {
   return (
     <View style={styles.container}>
-        <TextInput 
-            placeholder='Digite algo...'
+        <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field: { onChange, onBlur, value }}) => (
+            <TextInput 
+            placeholder={placeholder}
+            onBlur={onBlur}
+            value={value}
+            onChangeText={onChange}
+            keyboardType='default'
+        />
+        )}
         />
     </View>
   )
