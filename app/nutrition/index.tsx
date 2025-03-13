d96ba2ea93b1a1d238bd3f566534c6cc46dc9c47
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-13 14:54:23 -0300

    feat: create data types

--- app/nutrition/index.tsx
+++ app/nutrition/index.tsx
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { useDataStore } from '@/store/data'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/services/api'
+import { colors } from '@/constants/colors'
 
 export default function Nutrition() {
     const user = useDataStore(state => state.user)
     
-    console.log(user)
+    const { data, isFetching, error } = useQuery({
+        queryKey: ["nutrition"],
+        queryFn: async () => {
+            try {
+                if(!user){
+                    throw new Error("Failed to load nutrition")
+                }
+            const response = await api.get("/teste")
+
+            console.log(response.data.data)
+            return response.data.data
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    })
+
+    if(isFetching){
+        return(
+            <View style={styles.loading}>
+                <Text>Estamos gerando sua dieta!</Text>
+                <Text>Consultando IA.....</Text>
+            </View>
+        )
+    }
 
   return (
     <View>
@@ -13,3 +40,16 @@ export default function Nutrition() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    loading:{
+        flex:1,
+        backgroundColor: colors.background
+    },
+    loadingText:{
+        fontSize:18,
+        color:colors.white,
+        marginBottom:4,
+        justifyContent: 'center'
+    }
+})
